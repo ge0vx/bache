fcbdff0eb4c83b63d7047ec20b2442bbeac4057d
giovanyperezbautista <geovapb@gmail.com>
2021-10-13 05:40:30 -0500

    confire map for ios

--- app/home/index.js
+++ app/home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import {connect} from 'react-redux';
 import {GET_ALL_USER_INFO_REQUEST} from '../models/user/actions';
 import MapView from 'react-native-maps';
@@ -20,17 +20,32 @@ const mapDispatchToProps = (dispatch, props) => ({
 
 const HomeScreen = ({id, name, email, getAllUserInfo}) => {
   return (
-    <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
   );
 };
 
-const Home = connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
+const Home = connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
 export {Home};
